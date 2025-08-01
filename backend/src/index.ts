f2bd9869e6a04c5b3a6dc303dcfdd27e9086b3ca
Pratik Kamble <prak@ciklum.com>
2025-08-01 13:02:04 +0530

    vercel config change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -42,11 +42,11 @@ app.use(`${BASE_PATH}/mfa`, mfaRoutes);
 
 app.use(errorHandler);
 
-// app.listen(config.PORT, async () => {
-//   console.log(`Server is running on ${config.APP_ORIGIN}:${config.PORT}`);
-//   await connectDB();
-// });
+app.listen(config.PORT, async () => {
+  console.log(`Server is running on ${config.APP_ORIGIN}:${config.PORT}`);
+  await connectDB();
+});
 
-connectDB();
+// connectDB();
 
-export default app;
+// export default app;
